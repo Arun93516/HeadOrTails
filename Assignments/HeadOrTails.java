29c3ed416e354ce05f66e5d7c9226201eee5f2f1
Arun93516 <arunkumar93516@gmail.com>
2022-09-11 18:19:30 +0530

    Added : Welcome to Coin Fliping

---
+++ Assignments/HeadOrTails.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+import java.util.Random;
+
+public class HeadOrTails {
+	public static void main(String args[]) {
+		Random r = new Random();
+		int chance = r.nextInt(2);
+		if (chance == 1) {
+			System.out.println("Getting High percentage in Tails ");
+		} else {
+			System.out.println("Getting High percenttage in Heads ");
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+This program just like ah Coin fliping we can use the the random method use to get the Coin is Head Or Tail we finding 
