dbd4e86bfe01de67cae9e8965ee072981f285d40
Arun93516 <arunkumar93516@gmail.com>
2022-09-11 18:33:12 +0530

    Added: Welcome to Leap year program

--- Assignments/HeadOrTails.java
+++ Assignments/HeadOrTails.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class HeadOrTails {
 	public static void main(String args[]) {
 		Random r = new Random();
-		int chance = r.nextInt(2);
-		if (chance == 1) {
+		int RandomNumber = r.nextInt(2);
+		if (RandomNumber == 1) {
 			System.out.println("Getting High percentage in Tails ");
 		} else {
 			System.out.println("Getting High percenttage in Heads ");
